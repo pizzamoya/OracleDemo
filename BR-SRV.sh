66b7e4dd6eddd9d24e135905c44ef935cb65e19b
pizzamoya <151932541+pizzamoya@users.noreply.github.com>
2024-04-01 17:13:36 +0400

    BR-SRV.sh

--- BR-SRV.sh
+++ BR-SRV.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-rm -rf /etc/net/ifaces/ens18
+rm -rf /etc/net/ifaces/enp0s3
  
 echo "nameserver 77.88.8.8" >> /etc/resolv.conf
-mkdir /etc/net/ifaces/ens18
-cat <<EOF > /etc/net/ifaces/ens18/options
+mkdir /etc/net/ifaces/enp0s3
+cat <<EOF > /etc/net/ifaces/enp0s3/options
 TYPE=eth
 DISABLED=no
 NM_CONTROLLED=no
@@ -13,13 +13,13 @@ IPV6_CONFIG=yes
 EOF
  
 sed -i 's/CONFIG_IPV6=no/CONFIG_IPV6=yes/g' /etc/net/ifaces/default/options
-mkdir /etc/net/ifaces/ens18
+mkdir /etc/net/ifaces/enp0s3
  
 
-echo 192.168.200.1/28 > /etc/net/ifaces/ens18/ipv4address
-echo 2000:200::1/124 > /etc/net/ifaces/ens18/ipv6address
-echo default via 192.168.200.14 > /etc/net/ifaces/ens18/ipv4route
-echo default via 2000:200::f > /etc/net/ifaces/ens18/ipv6route  
+echo 192.168.200.1/28 > /etc/net/ifaces/enp0s3/ipv4address
+echo 2000:200::1/124 > /etc/net/ifaces/enp0s3/ipv6address
+echo default via 192.168.200.14 > /etc/net/ifaces/enp0s3/ipv4route
+echo default via 2000:200::f > /etc/net/ifaces/enp0s3/ipv6route  
  
 sed -i '10a\net.ipv6.conf.all.forwarding = 1' /etc/net/sysctl.conf
 sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/net/sysctl.conf
